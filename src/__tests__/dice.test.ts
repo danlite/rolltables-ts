ac3381460e1739b856fac00dd3576693ff628e51
Dan Lichty <dan@northone.com>
2020-05-18 14:01:24 -0400

    fix tests

--- src/__tests__/dice.test.ts
+++ src/__tests__/dice.test.ts
@@ -1,5 +1,5 @@
 import chalk from 'chalk'
-import {evaluatePlaceholders} from '../rolltables'
+import {evaluatePlaceholders} from '../evaluate'
 import {parseDice} from '../parse'
 
 describe('parse dice strings', () => {
